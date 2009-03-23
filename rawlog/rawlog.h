1281c9e349d9e63023b773149058b2c1fe43f027
Bogdan Opanchuk <bg@bk.ru>
2009-03-23 13:55:50 +0300

    rawlog: added descriptions about thread safety

--- rawlog/rawlog.h
+++ rawlog/rawlog.h
@@ -1,10 +1,15 @@
 #ifndef _RAWLOG_H
 #define _RAWLOG_H
 
+// Not a thread-safe
 int RawLogStartA(size_t index, const char *file_name);
 int RawLogStartW(size_t index, const wchar_t *file_name);
+
+// Thread-safe, if log is started
 void RawLogWriteA(size_t index, const char *message);
 void RawLogWriteW(size_t index, const wchar_t *message);
+
+// Not a thread-safe
 void RawLogStop(size_t index);
 
 #ifdef UNICODE
