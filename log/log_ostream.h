cdccfd576628696be64ea6257d7559f09179682c
Bogdan Opanchuk <bg@bk.ru>
2009-04-05 02:44:02 +0400

    log: 4 spaces replaced by tabs

--- log/log_ostream.h
+++ log/log_ostream.h
@@ -5,8 +5,8 @@
 #include <streambuf>
 
 /*!
-    \brief Functor definition for output policy
-    \param charT type of characters to handle (char or wchar_t)
+	\brief Functor definition for output policy
+	\param charT type of characters to handle (char or wchar_t)
 */
 template<class charT>
 class log_functor 
@@ -15,124 +15,124 @@ class log_functor
 };
 
 /*!
-    \brief Userdefined stream buffer for debug logging
-    \param charT type of characters to handle (char or wchar_t)
-    \param logfunction Functor type for output policy
-    \param traits Character traits 
+	\brief Userdefined stream buffer for debug logging
+	\param charT type of characters to handle (char or wchar_t)
+	\param logfunction Functor type for output policy
+	\param traits Character traits 
 */
 template
 <
-    class charT,                                    // character type
-    class logfunction,                              // logfunction type
-    class traits = std::char_traits<charT>          // character traits 
+	class charT,									// character type
+	class logfunction,							  // logfunction type
+	class traits = std::char_traits<charT>		  // character traits 
 >
 class log_streambuf : public std::basic_streambuf<charT, traits>
 {
-    typedef std::basic_string<charT, traits> string_type;   // shortcut for a string 
+	typedef std::basic_string<charT, traits> string_type;   // shortcut for a string 
 
 public:
-    virtual ~log_streambuf()
-    {
+	virtual ~log_streambuf()
+	{
 		sync();
-    }
+	}
 
-    //! \brief Set a context to be displayed before the messages 
-    void setContext(const string_type &context)
-    {
-        context_ = context;
-    }
+	//! \brief Set a context to be displayed before the messages 
+	void setContext(const string_type &context)
+	{
+		context_ = context;
+	}
 
 protected:
 
 	virtual typename traits::int_type overflow (typename traits::int_type c)
 	{
 		if (!traits::eq_int_type(c, traits::eof())) // if current character is not EOF 
-			buffer_ += traits::to_char_type(c);     // add current character to buffer
+			buffer_ += traits::to_char_type(c);	 // add current character to buffer
 
-		return traits::not_eof(c);              
+		return traits::not_eof(c);			  
 	}
 
-    virtual int sync()
-    {
-        if (!buffer_.empty())   // if characters in the buffer  
-            sendToDebugLog();   // send them to the debuglogfunction 
-        buffer_.clear();        // empty buffer
-        return 0;
-    }
+	virtual int sync()
+	{
+		if (!buffer_.empty())   // if characters in the buffer  
+			sendToDebugLog();   // send them to the debuglogfunction 
+		buffer_.clear();		// empty buffer
+		return 0;
+	}
 
 private:
-    string_type buffer_, context_;
+	string_type buffer_, context_;
 	logfunction func_;					//! the logfunction functor
 
-    //! \brief send the current stream content to the output policy 
-    void sendToDebugLog()
-    {
-        func_(context_ + buffer_);   // send context and message 
-    }
+	//! \brief send the current stream content to the output policy 
+	void sendToDebugLog()
+	{
+		func_(context_ + buffer_);   // send context and message 
+	}
 };
 
 /*!
-    \brief User defined stream type for debuglogging 
-    \param charT type of characters to handle (char or wchar_t)
-    \param logfunction Functor type for output policy
-    \param traits Character traits 
+	\brief User defined stream type for debuglogging 
+	\param charT type of characters to handle (char or wchar_t)
+	\param logfunction Functor type for output policy
+	\param traits Character traits 
 */
 template
 <
-    class charT,                                // character type 
-    class logfunction,                          // logfunction type
-    class traits = std::char_traits<charT>      // character traits 
+	class charT,								// character type 
+	class logfunction,						  // logfunction type
+	class traits = std::char_traits<charT>	  // character traits 
 >
 class log_ostream : public std::basic_ostream<charT, traits>
 {
-    typedef std::basic_string<charT, traits> string_type;                   // shortcut for a string
-    typedef log_streambuf<charT, logfunction, traits> buffer_type;     // shortcut for the streambuffer
-    typedef std::basic_ostream<charT, traits> stream_type;                  // shortcut for the stream 
-    typedef std::basic_ostringstream<charT, traits> stringstream_type;      // shortcut for a stringstream 
+	typedef std::basic_string<charT, traits> string_type;				   // shortcut for a string
+	typedef log_streambuf<charT, logfunction, traits> buffer_type;	 // shortcut for the streambuffer
+	typedef std::basic_ostream<charT, traits> stream_type;				  // shortcut for the stream 
+	typedef std::basic_ostringstream<charT, traits> stringstream_type;	  // shortcut for a stringstream 
 
 public:
-    /*!
-        \brief the default constructor
-        \param file the filename where the debuglogging occured
-        \param line the linenumber where the debuglogging occured
-    */
-    log_ostream(int level, const char *file = 0, int line = -1)
-        : level_(level), stream_type(&buf_), file_(file), line_(line)
-    {
-        buildContext(); // build the whole context and set it to the streambuffer
-    }
+	/*!
+		\brief the default constructor
+		\param file the filename where the debuglogging occured
+		\param line the linenumber where the debuglogging occured
+	*/
+	log_ostream(int level, const char *file = 0, int line = -1)
+		: level_(level), stream_type(&buf_), file_(file), line_(line)
+	{
+		buildContext(); // build the whole context and set it to the streambuffer
+	}
 
-    // allow deriving 
+	// allow deriving 
 	virtual ~log_ostream() {}
 
 	log_ostream &get() {return *this;}
 
 private:
-    // don't allow copying of the stream
-    log_ostream(const log_ostream &);
-    log_ostream &operator=(const log_ostream &);
+	// don't allow copying of the stream
+	log_ostream(const log_ostream &);
+	log_ostream &operator=(const log_ostream &);
 
-    /*!
-        \brief  Build the prefix for the debugmessage
-                [<filename>][(<linenumber>) : ][<context> : ]
-    */
-    void buildContext()
-    {
-        stringstream_type os;			// format the prefix via stringstream 
+	/*!
+		\brief  Build the prefix for the debugmessage
+				[<filename>][(<linenumber>) : ][<context> : ]
+	*/
+	void buildContext()
+	{
+		stringstream_type os;			// format the prefix via stringstream 
 
-        if (file_)                      // if a filename is given
-            os << file_;                // show it first
-        if (line_ >= 0)                 // if a linenumber is given
-            os << "(" << line_ << ")";  // show it in brackets
-        if (file_ || line_ >= 0)        // if filename or linenumber given
-            os << " : ";                // separate it by a double colon from context or message
-        buf_.setContext(os.str());      // set the context to the streambuffer
-    }
+		if (file_)					  // if a filename is given
+			os << file_;				// show it first
+		if (line_ >= 0)				 // if a linenumber is given
+			os << "(" << line_ << ")";  // show it in brackets
+		if (file_ || line_ >= 0)		// if filename or linenumber given
+			os << " : ";				// separate it by a double colon from context or message
+		buf_.setContext(os.str());	  // set the context to the streambuffer
+	}
 
 	const int level_;
-    const char *file_;					//! name of the sourcefile where the debuglogger was instantiated.
-    const int line_;					//! number of the line in file_ where the debuglogger was instantiated.
-    buffer_type buf_;					//! the streambuffer which sends it's content to OutputDebugString
+	const char *file_;					//! name of the sourcefile where the debuglogger was instantiated.
+	const int line_;					//! number of the line in file_ where the debuglogger was instantiated.
+	buffer_type buf_;					//! the streambuffer which sends it's content to OutputDebugString
 };
 
 #endif 
