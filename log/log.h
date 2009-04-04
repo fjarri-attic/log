b559b966d97f6cc4a84448ad1befdf1b4648ae53
Bogdan Opanchuk <bg@bk.ru>
2009-04-05 02:00:21 +0400

    log: create parameterized functors for file output and console output

--- log/log.h
+++ log/log.h
@@ -11,17 +11,74 @@
 #include <iostream>
 #include <fstream>
 
-template<class charT>
+// Output to file
+template<class charT, int FileID> 
 class log_to_file : public log_functor<charT>
 {
 public:
-	void operator()(std::basic_string<charT> &message)
-	{
-		RawLogWrite(0, message.c_str());
-	}
+	void operator()(std::basic_string<charT> &message);
 };
 
-typedef log_ostream<TCHAR, log_to_file<TCHAR> > DebugLogger;
+template<int FileID> 
+class log_to_file<char, FileID> : public log_functor<char>
+{
+public:
+	void operator()(std::basic_string<char> &message)
+	{
+		RawLogWriteA(FileID, message.c_str());
+	}	
+};
+
+template<int FileID> 
+class log_to_file<wchar_t, FileID> : public log_functor<wchar_t>
+{
+public:
+	void operator()(std::basic_string<wchar_t> &message)
+	{
+		RawLogWriteW(FileID, message.c_str());
+	}	
+};
+
+
+// Output to stderr
+template<class charT>
+class log_to_stderr : public log_functor<charT>
+{
+public:
+	void operator()(std::basic_string<charT> &message);
+};
+
+void log_to_stderr<char>::operator()(std::basic_string<char> &message)
+{
+	std::cerr << message << std::endl;
+}
+
+void log_to_stderr<wchar_t>::operator()(std::basic_string<wchar_t> &message)
+{
+	std::wcerr << message << std::endl;
+}
+
+// Output to stdout
+template<class charT>
+class log_to_stdout : public log_functor<charT>
+{
+public:
+	void operator()(std::basic_string<charT> &message);
+};
+
+void log_to_stdout<char>::operator()(std::basic_string<char> &message)
+{
+	std::cout << message << std::endl;
+}
+
+void log_to_stdout<wchar_t>::operator()(std::basic_string<wchar_t> &message)
+{
+	std::wcout << message << std::endl;
+}
+
+
+//typedef log_ostream<TCHAR, log_to_file<TCHAR, 0> > DebugLogger;
+typedef log_ostream<TCHAR, log_to_stderr<TCHAR> > DebugLogger;
 
 
 #endif
