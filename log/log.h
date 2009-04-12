fbbc97c68570182e7e1317606d175928882dc614
Bogdan Opanchuk <bg@bk.ru>
2009-04-12 11:33:41 +0400

    log: added combined logging functor

--- log/log.h
+++ log/log.h
@@ -25,12 +25,29 @@ public:
 	}
 };
 
-// Output to stderr
+// Output to stderr and stdout
 template<class charT>
 class log_to_console : public log_functor<charT>
 {
 public:
 	void operator()(std::basic_string<charT> &message, LogLevel level);
 };
+
+// Mixed output
+template<class charT, int FileID>
+class log_combined : public log_functor<charT>
+{
+private:
+	log_to_file<charT, FileID> file_target;
+	log_to_console<charT> console_target;
+
+public:
+	void operator()(std::basic_string<charT> &message, LogLevel level)
+	{
+		file_target(message, level);
+		console_target(message, level);
+	}
+};
+
 
 #endif
